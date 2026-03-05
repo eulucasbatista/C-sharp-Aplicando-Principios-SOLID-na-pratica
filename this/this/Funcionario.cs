45f7093955d1e1265ece3a92a874a19cac414029
eulucasbatista <lucas.batista20@gmail.com>
2026-03-05 19:55:18 -0300

    Subindo os arquivos das aulas

---
+++ this/this/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _This
+{
+    class Funcionario
+{
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Funcionario(string Nome, string Email)
+        {
+            //Use this para qualificar as propriedades
+            //Nome e Email evitando confusão com os nomes
+            // dos parâmetros usados no construtor
+
+            this.Nome = Nome;
+            this.Email = Email;
+        }
+    }
+}
---
+++ this/this/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _This
+{
+        class Teste
+        {
+                public string Nome { get; set; }
+                public Teste(Exemplo exemplo)
+                {
+                    // Usa o nome da instância Perl
+                    this.Nome = exemplo.Nome;
+                    Console.WriteLine($"Teste : {this.Nome}");
+                }
+        }
+        class Exemplo
+        {
+
+            public string Nome { get; set; }
+
+            public Exemplo(string nome)
+            {
+
+                this.Nome = nome;
+                // Passa a instância this como um parâmetro
+                Teste teste = new Teste(this);
+                // A instçância Teste agora possui o mesmo nome
+                Console.WriteLine($"Exemplo : {teste.Nome}");
+            }
+        }
+
+        class Program
+        {
+        
+            static void Main(string[] args)
+            {
+                Exemplo exemplo = new Exemplo("Eleven");
+                Console.ReadLine();
+            }
+        }
+}
